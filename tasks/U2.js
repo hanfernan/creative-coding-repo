0353a882f0b7c7d0c7d1d4df9e260be0288263ee
Hannah Fernandes-Martin <hfm11295@gmail.com>
2023-01-27 15:44:50 -0500

    completed u2 task 1

---
+++ tasks/U2.js
@@ -0,0 +1,11 @@
+//Write a function 
+//Create an array of strings with your favorite bands or artists; write a function called listArtists() which iterates over the items of the array and logs them out to the console.
+const favBands = ['Gorillaz', 'MF DOOM', 'The Garden', 'Nine Inch Nails', 'System of a Down', 'JPEGMAFIA', 'Doja Cat']
+const listArtists = () => {
+    for (let i = 0; i < favBands.length; i++ ) {
+        console.log(favBands[i]);
+    }
+}
+listArtists();
+//Invert
+//Invert the colors of the sketch. Use black for the background and white for the outlines.
