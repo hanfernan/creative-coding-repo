8ff86d1192bd5c16ea62186cd85efdd7a098337e
Hannah Fernandes-Martin <hfm11295@gmail.com>
2023-01-31 16:52:53 -0500

    transform lesson

---
+++ sketches/sketch-02.js
@@ -0,0 +1,40 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    context.fillStyle = "black";
+
+    const x = width * 0.5;
+    const y = height * 0.5;
+    const w = width * 0.3;
+    const h = height * 0.3;
+
+    //start with save
+    context.save();
+    context.translate(x,y);
+    context.rotate(0.3);
+    
+    context.beginPath();
+    context.rect(w * 0.5, h * 0.5, w, h);
+    context.fill();
+    context.restore();
+    //end with restore
+
+    context.translate(100, 400);
+
+    context.beginPath();
+    context.arc(0, 0, 50, 0, Math.PI * 2);
+    context.fill();
+
+
+  };
+};
+
+canvasSketch(sketch, settings);
