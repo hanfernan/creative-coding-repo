825daba37fa6327da17c8309cde35781e8643c7f
Hannah Fernandes-Martin <hfm11295@gmail.com>
2023-01-31 17:25:10 -0500

    util func lesson

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,12 +1,17 @@
 const canvasSketch = require('canvas-sketch');
-
+const math = require ('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
-const degToRad = (degrees) => {
-  return degrees/180 * Math.PI;
-};
+// const degToRad = (degrees) => {
+//   return degrees/180 * Math.PI;
+// };
+
+// const randomRange = (min, max) => {
+//   return Math.random() * (max - min) + min;
+// }
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -26,19 +31,20 @@ const sketch = () => {
     const radius = width * 0.3;
 
     for (let i = 0; i < num; i++) {
-      const slice = degToRad(360 / num);
+      const slice = math.degToRad(360 / num);
       const angle = slice * i;
 
       x = radius * Math.sin(angle);
       y = radius * Math.cos(angle);
       // x = cx + radius * Math.sin(angle);
       // y = cy + radius * Math.cos(angle);
-      
+
       //start with save
       context.save();
       context.translate(x, y);
       context.translate(cx, cy);
       context.rotate(-angle);
+      context.scale(random.range(1 , 3), 1);
 
       context.beginPath();
       context.rect(w * 0.5, h * 0.5, w, h);
